124c4a31ab23fb6214ff74b4430b369d7e94b8f6
matheus_gisch <matheus_gisch@sicredi.com.br>
2025-08-26 12:41:03 -0300

    Refactor layout structure by adding Container and Header components; update metadata for improved SEO and organization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Container from '@/components/Container';
-import Header from '@/components/Header';
 import PostFeatured from '@/components/PostFeatured';
 import PostList from '@/components/PostLists';
 import SpinLoader from '@/components/SpinLoader';
@@ -7,18 +5,11 @@ import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
-      <PostFeatured />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
         <PostList />
       </Suspense>
-
-      <footer>
-        <h1 className="text-4xl font-bold text-center py-8">FOOTER</h1>
-      </footer>
-    </Container>
+    </>
   );
 }
