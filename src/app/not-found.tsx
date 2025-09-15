a93e8acfcce39e2203160429743233ae93725c3b
matheus_gisch <matheus_gisch@sicredi.com.br>
2025-09-15 10:13:01 -0300

    feat: implement ErrorMessage component for consistent error handling and update NotFound component to utilize it

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,11 @@
-import Link from 'next/link';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFound() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className={`
-      min-h-[320px] bg-slate-900 text-slate-100 
-      mb-16 p-8 rounded-xl flex items-center justify-center
-      text-center
-    `}
-      >
-        <div>
-          <h1 className="text-7xl mb-4 font-bold">404</h1>
-          <p>A página que você está procurando não foi encontrada.</p>
-          <Link href="/" className="text-blue-500 hover:underline">
-            Voltar para a página inicial
-          </Link>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle="Página não encontrada"
+      contentTitle="404 😅"
+      content={<p>A página que você está procurando não foi encontrada.</p>}
+    />
   );
 }
